c01016e69370eec123a85e11697497e6aa8ede63
HimanshuMahajan007 <mahajahi@sheridancollege.ca>
2023-10-07 23:09:50 -0400

    Dashboard completed Project

---
+++ assets/files/javascript/database.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'iPhone 15 Pro Max',
+        productNumber: 'IME-19z23a032',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'iPad Pro',
+        productNumber: 'IME-e12op2o23',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Mac Pro',
+        productNumber: 'IME-m1sm2343m',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Macbook Pro',
+        productNumber: 'IME-m22a1a411',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Vision Pro',
+        productNumber: 'IME-v132vp141v',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Airpods Pro',
+        productNumber: 'IME-a12nn18r1',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    }
+]
